7be7965722fa77ada4419616050f25a78b4f2019
Alex Dean <alex@crackpot.org>
2011-03-30 09:35:37 -0500

    fix errors in paths to xml files sent back by server

--- metric_collection/php/test/scripts/test_gmetad.rb
+++ metric_collection/php/test/scripts/test_gmetad.rb
@@ -7,21 +7,23 @@ loop {
   client = server.accept
   request = client.recv(1024).strip
   
-  filename = case request
+  filename = xml_dir+'/'
+  filename += case request
   when '/?filter=summary'
     "meta.xml"
   when '/cluster1'
     "cluster.xml"
   when '/'
-    "index_array"
-  when '/cluster1/?filter=summary'  
+    "index_array.xml"
+  when '/cluster1?filter=summary'  
     "cluster-summary.xml"
   when '/cluster1/cluster1-host1'
     "node.xml"
   end
   
   puts "#{Time.now}: #{request} -> #{filename}"
-  response = File.exists?( filename ) ? File.read( filename ) : ""
+  response = File.exists?( filename.to_s ) ? File.read( filename ) : ""
+  puts response
   client.puts( response )
   client.close
 }
